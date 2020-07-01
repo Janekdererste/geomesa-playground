b5061e076bb5d50fec399f2401d32581c81f0f36
Janekdererste <laudan@tu-berlin.de>
2020-07-01 12:38:40 +0200

    * Remove final from AppConfiguration so that jackson can populate its values * Add cors stuff, which I still don't understand

--- server/src/main/java/org/matsim/contribs/analysis/App.java
+++ server/src/main/java/org/matsim/contribs/analysis/App.java
@@ -1,9 +1,15 @@
 package org.matsim.contribs.analysis;
 
 import io.dropwizard.Application;
+import io.dropwizard.jetty.setup.ServletEnvironment;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import lombok.extern.slf4j.Slf4j;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import java.util.EnumSet;
 
 @Slf4j
 public class App extends Application<AppConfiguration> {
@@ -30,5 +36,19 @@ public class App extends Application<AppConfiguration> {
         log.info("Registering resources");
         var networkResource = new NetworkResource(store);
         environment.jersey().register(networkResource);
+
+        registerCORSFilter(environment.servlets());
+    }
+
+    private void registerCORSFilter(ServletEnvironment servlet) {
+
+        final FilterRegistration.Dynamic cors = servlet.addFilter("CORS", CrossOriginFilter.class);
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "GET, POST, PUT, OPTIONS, DELETE, PATCH");
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_HEADERS_PARAM, "Authorization, Content-Type");
+        cors.setInitParameter(CrossOriginFilter.ALLOW_CREDENTIALS_PARAM, "true");
+
+        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+        cors.setInitParameter(CrossOriginFilter.CHAIN_PREFLIGHT_PARAM, Boolean.FALSE.toString());
     }
 }
